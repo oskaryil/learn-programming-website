d6f77b2093e39d2699c026892e7dbcde111b4766
martinnord <martin.nordstrom99@gmail.com>
2018-10-17 22:38:24 +0200

    Ignore eslint rule since we don't use prop-types. Can now view the selected route in the navbar

--- src/elements/Navbar.js
+++ src/elements/Navbar.js
@@ -2,17 +2,14 @@ import React from 'react';
 import { AppBar, NavItem } from '../atoms';
 import routes from '../mocks/routes';
 
-const Navbar = () => {
-  // const routeIsActive = props.location.pathname === props.match.path;
-  return (
-    <AppBar>
-      {routes.map(route => (
-        <NavItem key={route.path} to={route.path}>
-          {route.name}
-        </NavItem>
-      ))}
-    </AppBar>
-  );
-};
+const Navbar = () => (
+  <AppBar>
+    {routes.map(route => (
+      <NavItem key={route.path} to={route.path} exact activeClassName="active-nav-item">
+        {route.name}
+      </NavItem>
+    ))}
+  </AppBar>
+);
 
 export default Navbar;
